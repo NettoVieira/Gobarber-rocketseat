f797d5ca2598b6965faaaf9aa25111f73d553518
NettoVieira <joao.fvneto@hotmail.com>
2020-02-05 20:44:11 -0300

    configurando nodemon e sucrase mais as configurações de Debug

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
- return res.json({message: "Ola mundo"});
+ return res.json({message: "Hello teste"});
 });
 
-module.exports = routes;
+export default routes;
