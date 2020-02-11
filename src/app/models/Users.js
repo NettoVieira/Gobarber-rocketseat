536aacf34521f49bd40cf6a02c08a4281caa6240
NettoVieira <joao.fvneto@hotmail.com>
2020-02-11 06:48:56 -0300

    inserindo um usuario no banco de dados e antes de salvar a senha utilizamos o bcrypt para a criptografia

---
+++ src/app/models/Users.js
@@ -0,0 +1,29 @@
+import Sequelize, { Model } from 'sequelize';
+
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async users => {
+      if (users.password) {
+        users.password_hash = await bcrypt.hash(users.password, 8);
+      }
+    });
+    return this;
+  }
+}
+
+export default User;
